0e3a3158166d2ea912978b6c3db2e39a2e2c6fe3
Jeong Wook Park <hatchling3713@gmail.com>
2022-04-07 20:20:38 +0900

    Added RecyclerView, ViewHolder, Model for CommunityFragment Adjusted height of AppBar layout

--- app/src/main/java/com/example/shareroutine/ui/community/CommunityViewModel.kt
+++ app/src/main/java/com/example/shareroutine/ui/community/CommunityViewModel.kt
@@ -1,7 +1,24 @@
 package com.example.shareroutine.ui.community
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.shareroutine.data.model.Routine
 
+//Temporary
 class CommunityViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val routineList = MutableLiveData<List<Routine>>()
+
+    private val data = arrayListOf<Routine>()
+
+    fun addRoutine(routine: Routine) {
+        data.add(routine)
+
+        routineList.value = data
+    }
+
+    fun removeRoutine(routine: Routine) {
+        data.remove(routine)
+
+        routineList.value = data
+    }
 }
