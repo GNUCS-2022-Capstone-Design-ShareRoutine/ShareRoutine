c163067d1104f55ad31e162fb37fe286f74eeee6
harry123492 <harry123492@naevr.com>
2022-04-08 21:54:00 +0900

    firebase연동

--- app/src/main/java/com/example/shareroutine/MainActivity.kt
+++ app/src/main/java/com/example/shareroutine/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.example.shareroutine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.shareroutine.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        //val myRef : DatabaseReference = database.getReference("message")
+        //myRef.setValue("안녕 반가워!")
+
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
