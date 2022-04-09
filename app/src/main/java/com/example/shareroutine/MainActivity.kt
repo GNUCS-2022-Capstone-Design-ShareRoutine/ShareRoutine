b581fd61742cad268bbd3881a4ff7ee4d7cb7a13
Jeong Wook Park <hatchling3713@gmail.com>
2022-04-09 21:15:05 +0900

    Gradle plugin update Redundant code removed

--- app/src/main/java/com/example/shareroutine/MainActivity.kt
+++ app/src/main/java/com/example/shareroutine/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.shareroutine
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,26 +8,17 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.shareroutine.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        //val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-        //val myRef : DatabaseReference = database.getReference("message")
-        //myRef.setValue("안녕 반가워!")
-
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
