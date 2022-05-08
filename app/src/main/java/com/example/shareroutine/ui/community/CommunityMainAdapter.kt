e44ebb504aa265ee075642b598e88019dc03fc74
Jeong Wook Park <hatchling3713@gmail.com>
2022-05-08 16:56:31 +0900

    Merge 이후 에러 수정

--- app/src/main/java/com/example/shareroutine/ui/community/CommunityMainAdapter.kt
+++ app/src/main/java/com/example/shareroutine/ui/community/CommunityMainAdapter.kt
@@ -13,10 +13,10 @@ import com.example.shareroutine.databinding.CommunityCardViewBinding
 class CommunityMainAdapter(private var postList: List<Post>) : RecyclerView.Adapter<CommunityMainAdapter.CommunityMainViewHolder>() {
     class CommunityMainViewHolder(val binding: CommunityCardViewBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommunityMainViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.community_card_view, parent, false)
 
-        return ViewHolder(CommunityCardViewBinding.bind(view))
+        return CommunityMainViewHolder(CommunityCardViewBinding.bind(view))
     }
     
     override fun onBindViewHolder(holder: CommunityMainViewHolder, position: Int) {
